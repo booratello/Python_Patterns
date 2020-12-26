70848bb086d939e116ce87e9269e6721badcde98
booratello <booratello@mail.ru>
2020-12-27 00:21:01 +0600

    Домашнее задание к уроку 6 "Поведенческие паттерны"

---
+++ homework_6/framework/core.py
@@ -0,0 +1,83 @@
+import chardet
+
+
+class Application:
+
+    def add_route(self, url):
+        # паттерн декоратор
+        def inner(view):
+            self.urlpatterns[url] = view
+        return inner
+
+    def decoder(self, some_str):
+        result = chardet.detect(some_str)
+        some_str = some_str.decode(result['encoding']).encode('utf-8').decode('utf-8')
+        return some_str
+
+    def parse_input_data(self, data: str):
+        result = {}
+        if data:
+            params = data.split('&')
+            for item in params:
+                key, value = item.split('=')
+                result[key] = value
+        return result
+
+    def parse_wsgi_input_data(self, data: bytes):
+        result = {}
+        if data:
+            data_str = self.decoder(data)
+            result = self.parse_input_data(data_str)
+        return result
+
+    def get_wsgi_input_data(self, env):
+        content_length_data = env.get('CONTENT_LENGTH')
+        content_length = int(content_length_data) if content_length_data else 0
+        data = env['wsgi.input'].read(content_length) if content_length > 0 else b''
+        return data
+
+    def __init__(self, urlpatterns: dict, front_controllers: list):
+        """
+        :param urlpatterns: словарь связок url: view
+        :param front_controllers: список front controllers
+        """
+        self.urlpatterns = urlpatterns
+        self.front_controllers = front_controllers
+
+    def __call__(self, env, start_response):
+        # текущий url
+        path = env['PATH_INFO']
+
+        # добавление закрывающего слеша
+        if not path.endswith('/'):
+            path = f'{path}/'
+
+        # Получаем все данные запроса
+        method = env['REQUEST_METHOD']
+        data = self.get_wsgi_input_data(env)
+        data = self.parse_wsgi_input_data(data)
+
+        query_string = env['QUERY_STRING']
+        request_params = self.parse_input_data(query_string)
+
+        if path in self.urlpatterns:
+            # получаем view по url
+            view = self.urlpatterns[path]
+            request = {}
+            # добавляем параметры запросов
+            request['method'] = method
+            request['data'] = data
+            request['request_params'] = request_params
+            # добавляем в запрос данные из front controllers
+            for controller in self.front_controllers:
+                controller(request)
+            # вызываем view, получаем результат
+            code, text = view(request)
+            # возвращаем заголовки
+            start_response(code, [('Content-Type', 'text/html')])
+            # возвращаем тело ответа
+            return [text.encode('utf-8')]
+        else:
+            # Если url нет в urlpatterns - то страница не найдена
+            start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
+            return [b"Not Found"]
---
+++ homework_6/views.py
@@ -0,0 +1,125 @@
+from framework import render
+from models import TrainingSite, BaseSerializer, EmailNotifier, SmsNotifier
+from logging_mod import Logger, debug
+from framework.frameworkcbv import ListView, CreateView
+
+site = TrainingSite()
+logger = Logger('file')
+email_notifier = EmailNotifier()
+sms_notifier = SmsNotifier()
+
+
+@debug
+def main_view(request):
+    logger.log('Список курсов')
+    return '200 OK', render('course_list.html', objects_list=site.courses)
+
+
+@debug
+def category_list(request):
+    logger.log('Список категорий')
+    return '200 OK', render('category_list.html', objects_list=site.categories)
+
+
+@debug
+def create_course(request):
+    if request['method'] == 'POST':
+        # метод пост
+        data = request['data']
+        name = data['name']
+        category_id = data.get('category_id')
+        # print(category_id)
+        if category_id:
+            category = site.find_category_by_id(int(category_id))
+
+            course = site.create_course('record', name, category)
+            # Добавляем наблюдателей на курс
+            course.observers.append(email_notifier)
+            course.observers.append(sms_notifier)
+            site.courses.append(course)
+
+        categories = site.categories
+        return '200 OK', render('create_course.html', categories=categories)
+    else:
+        categories = site.categories
+        return '200 OK', render('create_course.html', categories=categories)
+
+
+# @debug
+# def create_category(request):
+#     if request['method'] == 'POST':
+#         data = request['data']
+#         name = data['name']
+#         category_id = data.get('category_id')
+#         category = None
+#         if category_id:
+#             category = site.find_category_by_id(int(category_id))
+#         new_category = site.create_category(name, category)
+#         site.categories.append(new_category)
+#         # print(site.categories)
+#         return '200 OK', render('create_category.html')
+#     else:
+#         categories = site.categories
+#         return '200 OK', render('create_category.html', categories=categories)
+
+@debug
+class CategoryCreateView(CreateView):
+    template_name = 'create_category.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['categories'] = site.categories
+        return context
+
+    def create_obj(self, data: dict):
+        name = data['name']
+        category_id = data.get('category_id')
+
+        category = None
+        if category_id:
+            category = site.find_category_by_id(int(category_id))
+
+        new_category = site.create_category(name, category)
+        site.categories.append(new_category)
+
+
+@debug
+class CategoryListView(ListView):
+    queryset = site.categories
+    template_name = 'category_list.html'
+
+
+@debug
+class StudentListView(ListView):
+    queryset = site.students
+    template_name = 'student_list.html'
+
+@debug
+class StudentCreateView(CreateView):
+    template_name = 'create_student.html'
+
+    def create_obj(self, data: dict):
+        name = data['name']
+        new_obj = site.create_user('student', name)
+        site.students.append(new_obj)
+
+@debug
+class AddStudentByCourseCreateView(CreateView):
+    template_name = 'add_student.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['courses'] = site.courses
+        context['students'] = site.students
+        return context
+
+    def create_obj(self, data: dict):
+        course_name = data['course_name']
+        course = site.get_course(course_name)
+        student_name = data['student_name']
+        student = site.get_student(student_name)
+        course.add_student(student)
+
+@debug
+def course_api(request):
+    return '200 OK', BaseSerializer(site.courses).save()
